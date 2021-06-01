04cffc3e5bf49269876692943ef7f5e33d82d5a3
Shishir Bhuiyan <60442502+ShishirBhuiyan@users.noreply.github.com>
2021-06-01 11:30:58 +0600

    .

--- Asset/js/app.js
+++ Asset/js/app.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     //<=========== Hamburger Click =============>
     $(".menu").click(function() {
         $(".menu .line").toggleClass("active");
+        $(".navigation").toggleClass("open");
     });
 
 });//<=========== Jquery End =============>
